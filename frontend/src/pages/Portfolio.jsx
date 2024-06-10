e10dedce98eadc9ffbb076919381cff0fa93d8f8
Remilekun Elijah <remilekunelijah21997@gmail.com>
2024-06-10 05:18:02 +0200

    implements services page

---
+++ frontend/src/pages/Portfolio.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header text="PORTFOLIO" />
+      <Footer />
+    </div>
+  );
+};
+
+export default Portfolio;
