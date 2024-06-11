d6119a71f599876358a04aef6549c9dfb7f3c215
Remilekun Elijah <remilekunelijah21997@gmail.com>
2024-06-11 11:33:34 +0100

    minor fixes

--- frontend/src/pages/Packages/Packages.jsx
+++ frontend/src/pages/Packages/Packages.jsx
@@ -37,7 +37,104 @@ const Packages = () => {
       a.type === "tubular" ? tubular.push(a) : lithium.push(a)
     )
   );
-  console.log(tubular);
+
+  function displayProduct(products) {
+    return products.map((a, i) => (
+      <div
+        key={i}
+        className={`shadow rounded-xl py-5 md:px-3 px-5 ${
+          a.type === "lithium" && a.name === "Platinum" ? "bg-red" : "bg-white"
+        }`}
+      >
+        <h4
+          className={`sora-semibold text-2xl mb-5 mt-2 text-center ${
+            a.type === "lithium" && a.name === "Platinum"
+              ? "text-white"
+              : "text-black"
+          }`}
+        >
+          {a.name}
+        </h4>
+
+        <p
+          className={`inter-medium md:text-base text-sm text-center mb-5 
+                      ${
+                        a.type === "lithium" && a.name === "Platinum"
+                          ? "text-white"
+                          : "md:text-[#EDA4A6] text-[#e26767]"
+                      }`}
+        >
+          {a.load}
+        </p>
+
+        <p className="text-center flex justify-center gap-2 mb-3 items-center">
+          <span className="inline-flex">
+            <strong
+              className={`md:text-[50px] text-4xl drop-shadow-md p-0 m-0 inter-semibold ${
+                a.type === "lithium" && a.name === "Platinum"
+                  ? "text-white"
+                  : "text-black"
+              }`}
+            >
+              {a.kva}
+            </strong>
+            <sup
+              className={` ${
+                a.type === "lithium" && a.name === "Platinum"
+                  ? "text-white"
+                  : "text-[#EDA4A6]"
+              } drop-shadow-md text-lg p-0 mt-2 ml-1 inter-medium`}
+            >
+              kva
+            </sup>
+          </span>
+          {a.volt && (
+            <span className="inline-flex">
+              <strong
+                className={`md:text-[50px] ${
+                  a.type === "lithium" && a.name === "Platinum"
+                    ? "text-white"
+                    : "text-black"
+                } text-4xl drop-shadow-md p-0 m-0 inter-semibold`}
+              >
+                {a.volt}
+              </strong>
+              <sup
+                className={` ${
+                  a.type === "lithium" && a.name === "Platinum"
+                    ? "text-white"
+                    : "text-[#EDA4A6]"
+                } drop-shadow-md text-lg p-0 mt-2 ml-1 inter-medium`}
+              >
+                v
+              </sup>
+            </span>
+          )}
+        </p>
+
+        <div className="bg-[#F9FAFB] rounded-xl px-5 py-7">
+          {a.options.map((b, i) => (
+            <div key={i} className="flex gap-3 mb-5">
+              <CheckIcon className="p-1 bg-red text-white rounded-full" />
+              <div>
+                <p className="inter-medium md:text-base text-sm">
+                  {b.name},{" "}
+                  <span className="text-red">₦{getAmount(b.price)}</span>
+                </p>
+                <p className="inter-medium md:text-base text-sm">({b.kits})</p>
+              </div>
+            </div>
+          ))}
+
+          <div className="flex justify-center mt-7">
+            <button className="shadow-lg inter-semibold text-base text-red bg-white rounded-xl py-4 px-10">
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    ));
+  }
   return (
     <div>
       <Navbar />
@@ -76,7 +173,7 @@ const Packages = () => {
           </div>
 
           <div className="grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 gap-10">
-            {active === 0
+            {/* {active === 0
               ? tubular.map((a, i) => (
                   <div
                     key={i}
@@ -225,7 +322,9 @@ const Packages = () => {
                       </div>
                     </div>
                   </div>
-                ))}
+                ))} */}
+
+            {displayProduct(active === 0 ? tubular : lithium)}
           </div>
         </Container>
       </div>
