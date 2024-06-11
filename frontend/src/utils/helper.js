c0bf2092e115c35a10fbdc0f1738f727b5087e09
Remilekun Elijah <remilekunelijah21997@gmail.com>
2024-06-11 10:04:46 +0100

    adds packages page

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -3,3 +3,5 @@ export const whatDrivesUs = [
   "Over the years we have powered various homes, offices and government institution across the country.  Our team are highly trained with vast experience, we sell and deploy only premium quality products.",
   "We are well known for our top notch professional installation and special love for premium quality. Our team are highly trained with vast experience, we sell and deploy only premium quality products.",
 ];
+
+export const getAmount = (num) => new Intl.NumberFormat().format(num);
