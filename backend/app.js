8830f7044a29987e1f7b804fa75e546aa8115406
Remilekun Elijah <remilekunelijah21997@gmail.com>
2024-06-15 15:36:18 +0100

    added options to cors config

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 if (app.get("env") === "development") env.config();
 
-app.use(cors({ origin: "*", methods: ["POST", "GET", "HEAD"] }));
+app.use(cors({ origin: "*", methods: ["POST", "GET", "HEAD", "OPTIONS"] }));
 app.use(express.json());
 
 app.use(userRouter);
