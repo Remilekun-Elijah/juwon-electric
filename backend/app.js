0c7fe9ec5b7059122d341a7f935496c12b5077a8
Remilekun Elijah <remilekunelijah21997@gmail.com>
2024-06-15 03:55:19 +0100

    adds cors allowed methods

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 if (app.get("env") === "development") env.config();
 
-app.use(cors());
+app.use(cors({ origin: "*", methods: ["POST", "GET", "HEAD"] }));
 app.use(express.json());
 
 app.use(userRouter);
