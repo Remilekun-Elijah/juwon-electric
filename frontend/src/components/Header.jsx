2fa1aa5b8cb812757eb7cc79526af595de09e86d
Remilekun Elijah <remilekunelijah21997@gmail.com>
2024-06-26 10:22:18 +0200

    changes header image to video

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,27 +3,51 @@ import { Link } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
 const Header = ({ text }) => {
   return (
-    <header className="services mt-16 flex flex-col justify-center items-center md:h-[400px] h-[250px]">
-      <h2 className="text-center inter-bold xl:text-6xl lg:text-5xl md:text-4xl text-3xl text-offWhite">
-        {text}
-      </h2>
+    <header className="mt-0 header-nav overflow-hidden max-h-[900px] lg:h-screen  h-[400px] w-full  relative">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="header-video h-full  w-full md:block hidden "
+      >
+        <source
+          src="/background_video_desktop.mp4"
+          className="md:block hidden"
+          type="video/mp4"
+        />
+      </video>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="header-video  h-screen  w-screen md:hidden block"
+      >
+        <source src="/background_video.mov" className="" type="video/mp4" />
+      </video>
+      <div className="header-content flex flex-col justify-center items-center lg:h-screen h-full w-full">
+        <h2 className="text-center inter-bold xl:text-6xl lg:text-5xl md:text-4xl text-3xl text-offWhite">
+          {text}
+        </h2>
 
-      <Link
-        to="/"
-        className="mt-10 px-20 py-3 sora-bold md:text-xl text-base text-white border-2"
-        style={{
-          background:
-            "radial-gradient(rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.2641), rgba(255, 255, 255, 0))",
-          filter: "drop-shadow(5px 10px 4px rgba(0, 0, 0, 0.5))",
-          border: `2px solid 
+        <Link
+          to="/"
+          className="mt-10 sm:px-20 px-10 py-3 sora-bold md:text-xl text-base text-white border-2"
+          style={{
+            background:
+              "radial-gradient(rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.2641), rgba(255, 255, 255, 0))",
+            filter: "drop-shadow(5px 10px 4px rgba(0, 0, 0, 0.5))",
+            border: `2px solid 
             radial-gradient(rgba(216, 216, 216, 0) rgba(216, 216, 216, 1))
             radial-gradient(rgba(216, 216, 216, 0) rgba(216, 216, 216, 1))
             linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0))
             `,
-        }}
-      >
-        GO HOME
-      </Link>
+          }}
+        >
+          GO HOME
+        </Link>
+      </div>
     </header>
   );
 };
