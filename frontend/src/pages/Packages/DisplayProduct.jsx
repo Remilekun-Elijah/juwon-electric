56aade745b0dca505c03790c75f18913d141acf2
remilekun-elijah <remilekunelijah21997@gmail.com>
2025-06-07 16:38:44 +0100

    What's new? - Update Lithium products price - Add new product type (Solar hybrid Lithium) - Add Solar hybrid lithium products

--- frontend/src/pages/Packages/DisplayProduct.jsx
+++ frontend/src/pages/Packages/DisplayProduct.jsx
@@ -1,6 +1,6 @@
 // import React from "react";
-import { getAmount } from "../../utils/helper";
 import CheckIcon from "@mui/icons-material/Check";
+import { getAmount } from "../../utils/helper";
 
 const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
   function openModal(product) {
@@ -16,19 +16,32 @@ const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
     });
 
     const inCart = a?.id === f?.id,
-      lithiumPlatinum = a.type === "lithium" && a.name === "Platinum",
-      tubularPremium = a.type === "tubular" && a.name === "Premium";
+      lithiumPlatinum = a.name === "Platinum",
+      lithiumDiamond = a.name === "Diamond",
+      tubularPremium = a.name === "Premium";
+
+    const diamondGradient = (dir) => {
+      return dir === "b"
+        ? `bg-gradient-to-${dir} from-[#ff6961] to-[#ff9f00]`
+        : `bg-gradient-to-${dir} from-[#ff9f00] to-[#ff6961]`;
+    };
 
     return (
       <div
         key={i}
         className={`shadow rounded-xl py-5 md:px-3 px-5 ${
-          lithiumPlatinum || tubularPremium ? "bg-red" : "bg-white"
+          lithiumPlatinum || tubularPremium
+            ? "bg-gradient-to-b to-pink-400 from-red"
+            : lithiumDiamond
+            ? diamondGradient("b")
+            : "bg-white"
         }`}
       >
         <h4
           className={`sora-semibold text-2xl mb-5 mt-2 text-center ${
-            lithiumPlatinum || tubularPremium ? "text-white" : "text-black"
+            lithiumPlatinum || tubularPremium || lithiumDiamond
+              ? "text-white"
+              : "text-black"
           }`}
         >
           {a.name}
@@ -37,7 +50,7 @@ const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
         <p
           className={`inter-medium md:text-base text-sm text-center mb-5 
                       ${
-                        lithiumPlatinum || tubularPremium
+                        lithiumPlatinum || tubularPremium || lithiumDiamond
                           ? "text-white"
                           : "md:text-[#EDA4A6] text-[#e26767]"
                       }`}
@@ -49,14 +62,16 @@ const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
           <span className="inline-flex">
             <strong
               className={`md:text-[50px] text-4xl drop-shadow-md p-0 m-0 inter-semibold ${
-                lithiumPlatinum || tubularPremium ? "text-white" : "text-black"
+                lithiumPlatinum || tubularPremium || lithiumDiamond
+                  ? "text-white"
+                  : "text-black"
               }`}
             >
               {a.kva}
             </strong>
             <sup
               className={` ${
-                lithiumPlatinum || tubularPremium
+                lithiumPlatinum || tubularPremium || lithiumDiamond
                   ? "text-white"
                   : "text-[#EDA4A6]"
               } drop-shadow-md text-lg p-0 mt-2 ml-1 inter-medium`}
@@ -68,7 +83,7 @@ const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
             <span className="inline-flex">
               <strong
                 className={`md:text-[50px] ${
-                  lithiumPlatinum || tubularPremium
+                  lithiumPlatinum || tubularPremium || lithiumDiamond
                     ? "text-white"
                     : "text-black"
                 } text-4xl drop-shadow-md p-0 m-0 inter-semibold`}
@@ -77,7 +92,7 @@ const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
               </strong>
               <sup
                 className={` ${
-                  lithiumPlatinum || tubularPremium
+                  lithiumPlatinum || tubularPremium || lithiumDiamond
                     ? "text-white"
                     : "text-[#EDA4A6]"
                 } drop-shadow-md text-lg p-0 mt-2 ml-1 inter-medium`}
@@ -91,16 +106,28 @@ const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
         <div
           className={`
           ${
-            lithiumPlatinum || tubularPremium ? "bg-white" : "bg-[#F9FAFB]"
+            lithiumPlatinum || tubularPremium || lithiumDiamond
+              ? "bg-white"
+              : "bg-[#F9FAFB]"
           } rounded-xl px-5 py-7`}
         >
           {a.options.map((b, i) => (
             <div key={i} className="flex gap-3 mb-5">
-              <CheckIcon className="p-1 bg-red text-white rounded-full" />
+              <CheckIcon
+                className={`p-1 ${
+                  lithiumDiamond ? "bg-[var(--diamond)]" : "bg-red"
+                } text-white rounded-full`}
+              />
               <div>
                 <p className="inter-medium md:text-base text-sm">
                   {b.name},{" "}
-                  <span className="text-red">₦{getAmount(b.price)}</span>
+                  <span
+                    className={`${
+                      lithiumDiamond ? "text-[var(--diamond)]" : "text-red"
+                    }`}
+                  >
+                    ₦{getAmount(b.price)}
+                  </span>
                 </p>
                 <p className="inter-medium md:text-base text-sm">({b.kits})</p>
               </div>
@@ -112,10 +139,14 @@ const DisplayProduct = ({ products, setOpen, setProduct, cart }) => {
               onClick={() => openModal(a)}
               disabled={inCart}
               className={` inter-semibold text-base hover:text-white text-red ${
-                inCart
+                inCart && lithiumDiamond
+                  ? "text-white bg-[#edbe71]"
+                  : inCart
                   ? "bg-[#EDA4A6] text-white"
                   : lithiumPlatinum || tubularPremium
-                  ? "text-white bg-red"
+                  ? "text-white bg-gradient-to-r to-pink-500 from-red"
+                  : lithiumDiamond
+                  ? `text-white ${diamondGradient("r")}`
                   : "bg-white hover:bg-red shadow-lg"
               }  rounded-lg py-4 px-10`}
             >
